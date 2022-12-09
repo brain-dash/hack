a0405af72cbcb0c593fdcbfa7ce7430459d2ea61
1NR_Operator_7 <1NR_Operator_7@era.mil.local>
2022-12-09 22:49:09 +0300

    Created page with drone view

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 ( ()=>{
     if (document.getElementById("init"))
         document.getElementById("init").onclick = ()=>window.location.href = 'select.html';
+    if (document.getElementById("btn-finish"))
+        document.getElementById("btn-finish").onclick = ()=>window.location.href = 'index.html';
 })()
