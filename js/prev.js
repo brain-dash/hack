a5f5f6ae77427b54052648bad07b876ee25b6106
1NR_Operator_7 <1NR_Operator_7@era.mil.local>
2022-12-09 11:53:43 +0300

    Prev button fixed

--- js/prev.js
+++ js/prev.js
@@ -31,8 +31,9 @@ class Prev extends HTMLElement {
     }
     css(){
       return /*css*/`<style> 
-      :host{
-      }
+        :host{
+          
+        }
         button{
           display: flex;
           justify-content: center;
@@ -40,21 +41,43 @@ class Prev extends HTMLElement {
           background:transparent;
           border:none;
           color:white;
+          cursor:pointer;
+          font-family: 'Montserrat';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 16px;
         }
         button p{
           padding-left:10px;
         }
-       
- 
+   
+        .border, .circle, .arrow{
+          transition:all 0.3s linear ;
+        }
+        button:hover .circle{
+          fill:var(--grey);
+        }
+        button:hover .arrow{
+          stroke:black;
+        }
+        button:active .circle{
+          fill:white;
+        }
+        button:active .arrow{
+          stroke:black;
+        }
     </style>`
     }
     html(){
         return /*html*/`
           <button onclick="history.back()">
           <svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M21.45 11.7L13.65 20.475L21.45 29.25" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-          <rect x="0.5" y="0.5" width="38" height="38" rx="19" stroke="white"/>
+            <rect class="circle" x="0.5" y="0.5" width="38" height="38" rx="19" fill="transparent"/>
+            <path class="arrow" d="M21.45 11.7L13.65 20.475L21.45 29.25" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+            <rect class="border" x="0.5" y="0.5" width="38" height="38" rx="19" stroke="white"/>
           </svg>
+
           <p>Назад</p>
           
           </button>
