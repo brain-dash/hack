4f0642773870c782d9b05a990cb834a3ff7dcba9
1NR_Operator_7 <1NR_Operator_7@era.mil.local>
2022-12-10 08:01:43 +0300

    Added link to main page

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@
         document.getElementById("init").onclick = ()=>window.location.href = 'select.html';
     if (document.getElementById("btn-finish"))
         document.getElementById("btn-finish").onclick = ()=>window.location.href = 'index.html';
+    if (document.getElementById("toMain"))
+        document.getElementById("toMain").onclick = ()=>window.location.href = 'index.html';
 })()
