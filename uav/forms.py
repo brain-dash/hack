40f14c7ce2c2b754383da73c65a8f63d16c74af1
Fedion1 <120024708+Fedion1@users.noreply.github.com>
2022-12-10 09:34:34 +0300

    initial commit
    initital commit

---
+++ uav/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+ 
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', )
+
+class SignInForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username','password',)
